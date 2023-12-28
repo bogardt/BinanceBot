a42f852f7b84308c2818547ad1697fa9ff89051f
Thomas <_thomas.bogard.ext.az@sodexo.com>
2023-12-28 20:20:30 +0100

    remove unused variable from Order model

--- BinanceBot/Model/Order.cs
+++ BinanceBot/Model/Order.cs
@@ -6,14 +6,7 @@ namespace BinanceBot.Model
     {
         [JsonProperty("symbol")]
         public required string Symbol { get; set; }
-
-        [JsonProperty("orderId")]
-        public long OrderId { get; set; }
-        [JsonProperty("price")]
-        public string Price { get; set; }
         [JsonProperty("side")]
         public string Side { get; set; }
-        [JsonProperty("time")]
-        public long Time { get; set; }
     }
 }
